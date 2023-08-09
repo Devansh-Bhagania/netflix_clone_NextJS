f778bd3051e630204e3a356ffebfd2af10e8c789
Devansh Bhagania <117916160+Devansh-Bhagania@users.noreply.github.com>
2023-08-09 22:57:55 +0530

    done done

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ export default function Home() {
     <h1 className='absolute text-2xl mx-auto align-item-center'>Hello world</h1>
     <p>hello brother today i am tired</p>
     <h2>tomorrow fa1 of CN</h2>
+    <h1>will start dsa from tomorrow</h1>
     </div>
   )
 }
