1ba19b5a9b73dd07b59c1b8543864f2de12ae8d2
Devansh Bhagania <117916160+Devansh-Bhagania@users.noreply.github.com>
2023-08-09 22:56:57 +0530

    new change in pagetsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <div className='relative mx-10'>
     <h1 className='absolute text-2xl mx-auto align-item-center'>Hello world</h1>
+    <p>hello brother today i am tired</p>
     </div>
   )
 }
