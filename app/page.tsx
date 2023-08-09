a8a0dcd360bf25dd738ed39e513daac7a8525d03
Devansh Bhagania <117916160+Devansh-Bhagania@users.noreply.github.com>
2023-08-09 22:57:30 +0530

    tomorro schedule

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ export default function Home() {
     <div className='relative mx-10'>
     <h1 className='absolute text-2xl mx-auto align-item-center'>Hello world</h1>
     <p>hello brother today i am tired</p>
+    <h2>tomorrow fa1 of CN</h2>
     </div>
   )
 }
